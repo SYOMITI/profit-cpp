e24f8c0b0cb68b17cd34d2c081f0c18d14853ee3
SYOMITI <115490512+SYOMITI@users.noreply.github.com>
2022-11-13 12:00:31 +0300

    Add files via upload

---
+++ profit/profit.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int buyingprice,sellingprice,profit;
+    cout<<"enter buyingprice:";
+    cin>>buyingprice;
+    cout<<"enter selling price:";
+    cin>>sellingprice;
+    profit=sellingprice-buyingprice;
+    cout<<"profit:"<<profit<<endl;
+}
